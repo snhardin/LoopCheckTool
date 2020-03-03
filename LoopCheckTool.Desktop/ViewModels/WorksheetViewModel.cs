2d5ab4943e5776d664058eb82f3f4eb0cbfbfac1
Scott Hardin <scottnhardin@gmail.com>
2020-03-02 22:35:25 -0500

    Port over in-progress interface

---
+++ LoopCheckTool.Desktop/ViewModels/WorksheetViewModel.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace LoopCheckTool.ViewModels
+{
+    public class WorksheetViewModel : INotifyPropertyChanged
+    {
+        private bool _hasValidData;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public ObservableCollection<ComboBoxItem> Worksheets { get; set; }
+        public bool HasValidData
+        {
+            get
+            {
+                return _hasValidData;
+            }
+            set
+            {
+                _hasValidData = value;
+                OnPropertyChanged("HasValidData");
+            }
+        }
+
+        protected void OnPropertyChanged(string name)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+
+        public WorksheetViewModel()
+        {
+            _hasValidData = false;
+            Worksheets = new ObservableCollection<ComboBoxItem>();
+        }
+    }
+}
