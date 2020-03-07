6447fb8b25ab65be3091b70c17f4f0f2d3772ea6
Scott Hardin <scottnhardin@gmail.com>
2020-03-07 15:09:34 -0500

    Revert "Port over in-progress interface"
    This reverts commit 2d5ab4943e5776d664058eb82f3f4eb0cbfbfac1.

--- LoopCheckTool.Desktop/MainWindow.xaml.cs
+++ LoopCheckTool.Desktop/MainWindow.xaml.cs
@@ -1,6 +1,4 @@
-﻿using LoopCheckTool.ViewModels;
-using Microsoft.Win32;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -15,77 +13,16 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace LoopCheckTool
+namespace LoopCheckTool.Desktop
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window
     {
-        //private ExcelLoopCheckReader reader;
-        public WorksheetViewModel WorksheetViewModel { get; }
-
         public MainWindow()
         {
             InitializeComponent();
-            WorksheetViewModel = new WorksheetViewModel();
-        }
-
-        private void SettingsCommand_Executed(object sender, ExecutedRoutedEventArgs e)
-        {
-            SettingsWindow settings = new SettingsWindow();
-            if (settings.ShowDialog() == true)
-            {
-                // Done.
-            }
-        }
-
-        private void SettingsCommand_CanExecute(object sender, CanExecuteRoutedEventArgs e)
-        {
-            e.CanExecute = true;
-        }
-
-        private void ExitCommand_Executed(object sender, ExecutedRoutedEventArgs e)
-        {
-            Application.Current.Shutdown();
-        }
-
-        private void ExitCommand_CanExecute(object sender, CanExecuteRoutedEventArgs e)
-        {
-            e.CanExecute = true;
-        }
-
-        private void OpenCommand_Executed(object sender, ExecutedRoutedEventArgs e)
-        {
-            OpenFileDialog dialog = new OpenFileDialog()
-            {
-                Filter = "All Files (*.*)|*.*",
-                Multiselect = false,
-            };
-
-            if (dialog.ShowDialog() == true)
-            {
-                //try
-                //{
-                //    reader = new ExcelLoopCheckReader(dialog.FileName);
-                //    ddlWorksheet.ItemsSource = reader.GetWorksheets().Select(s => new ComboBoxItem { Content = s });
-                //    ddlWorksheet.SelectedIndex = 0;
-                //    ddlWorksheet.IsEnabled = true;
-                //}
-                //catch (Exception ex)
-                //{
-                //    reader.Dispose();
-                //    reader = null;
-                //    ddlWorksheet.IsEnabled = false;
-
-                //    MessageBox.Show(ex.Message);
-                //}
-            }
-        }
-
-        private void OpenCommand_CanExecute(object sender, CanExecuteRoutedEventArgs e)
-        {
-            e.CanExecute = true;
         }
     }
 }
